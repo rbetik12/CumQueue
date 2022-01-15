b8fcce7c1288bf23c6167be2fa718462cc7a49bd
rbetik12 <mr.prikota@gmail.com>
2022-01-15 03:21:42 +0300

    Added cowboy response

--- src/producer_registrar/producer_register_controller.erl
+++ src/producer_registrar/producer_register_controller.erl
@@ -10,10 +10,31 @@
 -author("salvoroni").
 
 %% API
--export([init/2]).
+-export([init/2,
+  allowed_methods/2,
+  content_types_accepted/2, post_json/2]).
 
 init(Req0, Opts) ->
-  Req = cowboy_req:reply(200, #{
-    <<"content-type">> => <<"text/plain">>
-  }, <<"Hello in registration!">>, Req0),
-  {ok, Req, Opts}.
+  io:format("init~n"),
+  {cowboy_rest, Req0, Opts}.
+
+allowed_methods(Req, State) ->
+  io:format("methods~n"),
+  {[<<"POST">>, <<"GET">>], Req, State}.
+
+content_types_accepted(Req, State) ->
+  io:format("types~n"),
+  case cowboy_req:method(Req) of
+    <<"POST">> ->
+      Accepted = {[{<<"application/json">>, post_json}], Req, State};
+    <<"PUT">> ->
+      Accepted = {[{<<"application/json">>, put_json}], Req, State}
+  end,
+  Accepted.
+
+post_json(Req, State) ->
+  io:format("post_json: ~n"),
+  {ok, Data, Req1} = cowboy_req:read_body(Req),
+  Req2 = cowboy_req:set_resp_body(Data, Req1),
+  Req3 = cowboy_req:reply(200, Req2),
+  {stop, Req3, State}.
