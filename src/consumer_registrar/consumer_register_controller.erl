2281d35195d07cc6300bb478a34696902f2268ba
Iaremko Roman <salvoronis@gmail.com>
2022-01-15 22:13:34 +0300

    register consumer

--- src/consumer_registrar/consumer_register_controller.erl
+++ src/consumer_registrar/consumer_register_controller.erl
@@ -11,38 +11,40 @@
 
 %% API
 -export([init/2,
-  content_types_provided/2,
-  register_json/2,
+  register_consumer/2,
   allowed_methods/2,
-  content_types_accepted/2, post_json/2]).
+  content_types_accepted/2]).
+
+
+%% TODO апи для главного по топикам для отправки сообщения
 
 init(Req0, Opts) ->
   {cowboy_rest, Req0, Opts}.
 
-content_types_provided(Req, State) ->
-  {[
-    {<<"application/json">>, register_json}
-  ], Req, State}.
-
 allowed_methods(Req, State) ->
-  {[<<"POST">>, <<"GET">>], Req, State}.
-
-register_json(Req, State) ->
-  Has = cowboy_req:has_body(Req),
-  io:format("~w~n~n", [Has]),
-  io:format("~w~n~w~n", [Req, State]),
-  {<<"{\"rest\": \"Oh shit\"}">>, Req, State}.
+  {[<<"POST">>], Req, State}.
 
 content_types_accepted(Req, State) ->
   case cowboy_req:method(Req) of
     <<"POST">> ->
-      Accepted = {[{<<"application/json">>, post_json}], Req, State};
-    <<"PUT">> ->
-      Accepted = {[{<<"application/json">>, post_json}], Req, State}
-  end,
-  Accepted.
+      {[{<<"application/json">>, register_consumer}], Req, State}
+  end.
 
-post_json(Req, State) ->
+register_consumer(Req, State) ->
   {ok, Data, Req1} = cowboy_req:read_body(Req),
-  io:format("~w~n", [Data]),
-  {true, Req1, State}.
+  JsonData = jsone:decode(Data),
+  case consumer_registrar:register_consumer(#{
+    callback_url => maps:get(<<"url">>, JsonData),
+    topic => maps:get(<<"topic">>, JsonData),
+    group => maps:get(<<"group">>, JsonData)
+  }) of
+    {ok, {empty_topic}} -> Body = <<"{\"status\": \"OK\"}">>;
+    {ok, {already_exists}} -> Body = <<"{\"status\": \"already exists\"}">>;
+    {ok, {Messages}} -> Body = jsone:encode(Messages);
+    _ -> Body = <<"{\"status\": \"Cannot register consumer\"}">>
+  end,
+  Req2 = cowboy_req:set_resp_headers(#{
+    <<"content-type">> => <<"application/json">>
+  }, Req1),
+  Req3 = cowboy_req:reply(200, cowboy_req:resp_headers(Req2), Body, Req1),
+  {stop, Req3, State}.
--- src/consumer_registrar/consumer_registrar.erl
+++ src/consumer_registrar/consumer_registrar.erl
@@ -9,16 +9,27 @@
 -behaviour(gen_server).
 
 -export([start/0, stop/0]).
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
-  code_change/3]).
+-export([init/1,
+  handle_call/3,
+  handle_cast/2,
+  handle_info/2,
+  terminate/2,
+  code_change/3,
+  register_consumer/1]).
 
 -define(SERVER, ?MODULE).
 
--record(consumer_registrar_state, {}).
+-record(consumer_registrar_state, {consumers = maps:new(), groups = maps:new()}).
 
 %%%===================================================================
 %%% Spawning and gen_server implementation
 %%%===================================================================
+
+%%register_consumer(#{callback_url := Url, topic := Topic, group := Group} = ConsumerData) ->
+%%  .
+
+register_consumer(ConsumerDataMap) when is_map(ConsumerDataMap) ->
+  gen_server:call(consumer_registrar, {register_consumer, ConsumerDataMap}).
 
 start() ->
   io:format("Hello from consumer_reg!~n"),
@@ -30,6 +41,50 @@ stop() ->
 init([]) ->
 {ok, #consumer_registrar_state{}}.
 
+handle_call({register_consumer, #{callback_url := Url, topic := Topic, group := Group} = ConsumerDataMap},
+    _From,
+    State = #consumer_registrar_state{consumers = Consumers, groups = Groups}) ->
+  case maps:get(Url, Consumers, badkey) of
+    %% Consumer does not exists
+    badkey ->
+      GroupId = case maps:get(Group, Groups, notfound) of
+        notfound -> 0;
+        {ok, Id} -> Id
+      end,
+      %% Try to connect to topic
+      case topic_manager:get_topic_pid(Topic) of
+        %% No such topic
+        {notfound, _} ->
+          topic_manager:new_topic(Topic),
+          {reply, {ok, {empty_topic}}, State#consumer_registrar_state{
+            consumers = Consumers#{Url => Topic}
+          }};
+        %% Found topic
+        {ok, _} ->
+          case GroupId of
+            %% Return all data from topic
+            0 ->
+              %%Messages = topic_manager:get_all(Topic),
+              {Messages, MsgId} = not_implemented, not_implemented,
+              {reply, {ok, {Messages}}, State#consumer_registrar_state{
+                consumers = Consumers#{Url => Topic},
+                groups = Groups#{Group => MsgId}}
+              };
+            %% Return data after GroupId from topic
+            GroupId ->
+              %%Messages = topic_manager:get_from(Topic, GroupId),
+              {Messages, MsgId} = not_implemented, not_implemented,
+              {reply, {ok, {Messages}}, State#consumer_registrar_state{
+                consumers = Consumers#{Url => Topic},
+                groups = Groups#{Group := MsgId}}
+              }
+          end
+      end;
+    %% Consumer already exists
+    _ ->
+      lager:log(debug, self(), "Consumer with URL ~p for topic: ~p", [Url, Topic]),
+      {reply, {ok, {already_exists}}, State}
+  end;
 handle_call(_Request, _From, State = #consumer_registrar_state{}) ->
   {reply, ok, State};
 handle_call(stop, _From, Tab) ->
